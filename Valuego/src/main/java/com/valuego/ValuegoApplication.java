964a108ea0dd96c77af2dbb0b1b6fd6299d9f2e8
ttttkii913 <dbgkdud821@daum.net>
2026-08-08 04:05:17 +0900

    🎉 Feat: Project Initial Setting

---
+++ Valuego/src/main/java/com/valuego/ValuegoApplication.java
@@ -0,0 +1,13 @@
+package com.valuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuegoApplication.class, args);
+	}
+
+}
---
+++ Valuego/src/test/java/com/valuego/ValuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
