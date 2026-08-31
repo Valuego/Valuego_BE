89d6a3b13d1d0fc9dd029e36ec50b6a2e4c25e0a
ttttkii913 <dbgkdud821@daum.net>
2026-09-01 01:39:04 +0900

    :bug: Bug/Valuego/Valuego_BE#19: GuestToken Valid Logic fix

--- Valuego/src/main/java/com/valuego/global/common/exception/ValidMemberException.java
+++ Valuego/src/main/java/com/valuego/global/common/exception/ValidMemberException.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -20,16 +21,27 @@ public class ValidMemberException {
 
     // 로그인 사용자가 해당 그룹의 멤버인지 확인
     public GroupMember validateGroupMember(Principal principal, String guestToken, Group group) {
-        // 로그인 사용자
+        // 1. 로그인 사용자인 경우 검증
         if (principal != null) {
-            User user = entityFinderException.getUserFromPrincipal(principal);
+            try {
+                User user = entityFinderException.getUserFromPrincipal(principal);
+                Optional<GroupMember> groupMember = groupMemberRepository.findByGroupAndUser(group, user);
 
-            return groupMemberRepository.findByGroupAndUser(group, user).orElseThrow(
-                    () -> new BusinessException(ErrorCode.GROUP_MEMBER_NOT_FOUND_EXCEPTION, "해당 그룹의 멤버가 아닙니다."));
+                // 그룹 회원으로 등록되어 있으면 해당 멤버 반환
+                if (groupMember.isPresent()) {
+                    return groupMember.get();
+                }
+            } catch (Exception ignored) {
+            }
         }
 
-        // 게스트
-        return validateGuest(group, guestToken);
+        // 2. 로그인 멤버가 아니거나 회원 매핑이 실패한 경우, 게스트 토큰으로 2차 검증
+        if (guestToken != null && !guestToken.isBlank()) {
+            return validateGuest(group, guestToken);
+        }
+
+        // 3. 둘 다 실패한 경우
+        throw new BusinessException(ErrorCode.GROUP_MEMBER_NOT_FOUND_EXCEPTION, "해당 그룹의 멤버가 아닙니다.");
     }
 
     // 게스트 예외 검사
